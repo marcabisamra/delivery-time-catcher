c087163b20acc78cf2608995afb3420e16d729dd
Marc Abi-Samra <marc.abi-samra@moodys.com>
2020-04-09 08:07:46 -0400

    tries to maintain context to tab

--- scripts/background.js
+++ scripts/background.js
@@ -6,11 +6,21 @@ chrome.runtime.onInstalled.addListener(function() {
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.foundDeliveryTimes) {
-      chrome.windows.update(sender.tab.windowId, { focused: true }, function() {
-        chrome.tabs.update(sender.tab.id, { 'active': true }, function () {
-          console.log('Updated window and tab');
-        });
-      });
-    }
-  });
+   if (request.foundDeliveryTimes === true) {
+     chrome.windows.update(sender.tab.windowId, { focused: true }, function() {
+       chrome.tabs.update(sender.tab.id, { active: true }, function () {
+         console.log('Updated window and tab');
+         chrome.notifications.create({
+           type: 'basic',
+           iconUrl: '../assets/icon128.png',
+           title: 'Time to check out!',
+           message: 'Delivery times are now available'
+         }, function(notificationId) {
+         });
+       });
+     });
+
+    } else if (request.foundDeliveryTimes === false) {
+     console.log('No times found')
+   }
+});
--- scripts/content-scripts.js
+++ scripts/content-scripts.js
@@ -4,6 +4,7 @@ var foundDeliveryTimes = false;
 var CHECK_TIMES_INTERVAL = 12000;
 
 window.addEventListener('load', function () {
+  chrome.runtime.sendMessage({ foundDeliveryTimes: false });
   window.setInterval(checkForTimes, CHECK_TIMES_INTERVAL);
 });
 
@@ -16,7 +17,13 @@ function checkForTimes() {
       checkForTimesOnPrimeNow();
     }
   });
+}
 
+function didntFindTimesAction(){
+  console.log('No delivery times available');
+  chrome.runtime.sendMessage({ foundDeliveryTimes: false }, () => {
+    window.location.reload();
+  });
 }
 
 function checkForTimesOnInstacart() {
@@ -24,42 +31,43 @@ function checkForTimesOnInstacart() {
   if (deliveryOpts.length > 0) {
     foundTimesAction();
   } else {
-    window.location.reload();
+    didntFindTimesAction()
   }
 }
 
 function checkForTimesOnPrimeNow() {
-  var doesntHaveTimes = findEltByText('span', 'No delivery windows available. New windows are released throughout the day.');
-  if (doesntHaveTimes) {
-    console.log('No delivery times available');
-    window.location.reload();
-  } else {
+  var deliveryOpts = document.getElementsByName("delivery-window-radio");
+  if (deliveryOpts) {
     foundTimesAction();
+  } else {
+    didntFindTimesAction();
   }
 }
 
 function foundTimesAction() {
+  foundDeliveryTimes = true;
+  window.clearInterval(checkForTimes)
   var songUrl = chrome.runtime.getURL('/assets/Fairuz - El Bent El Shalabiah.mp3');
   var audioElt = document.createElement('audio');
   audioElt.src = songUrl;
   audioElt.onloadedmetadata = function () {
     chrome.runtime.sendMessage({ foundDeliveryTimes: true }, () => {
       audioElt.play();
-      foundDeliveryTimes = true;
     });
   };
-  window.clearInterval(checkForTimes)
 }
 
-function findEltByText(tag, text) {
-  var tags = document.getElementsByTagName(tag);
-  var isFound = false;
-
-  for (var i = 0; i < tags.length; i++) {
-    if (tags[i].textContent.trim() === text) {
-      isFound = true;
-      break;
-    }
-  }
-  return isFound;
-}
+// var doesntHaveTimes = findEltByText('span', 'Non delivery windows available. New windows are released throughout the day.');
+//
+// function findEltByText(tag, text) {
+//   var tags = document.getElementsByTagName(tag);
+//   var isFound = false;
+//
+//   for (var i = 0; i < tags.length; i++) {
+//     if (tags[i].textContent.trim() === text) {
+//       isFound = true;
+//       break;
+//     }
+//   }
+//   return isFound;
+// }
